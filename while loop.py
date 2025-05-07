2bcc22db9fdde0d14fe7213fe7b553ad6d2083fc
jeet332 <indrajeet3333333@gmail.com>
2025-05-07 12:53:50 +0530

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,48 @@
+# num=1
+# while(num<=5):
+#     print("num",num)
+#     num +=1
+#
+# A=2
+# while(A<5):
+#     print("A",A)
+#     A -=1
+
+# Multi=2
+# while(Multi<=20):
+#     print("Multi",Multi)
+#     Multi += 2
+
+# n=int(input("Enter your number: "))
+# num=1
+# while (num<=10):
+#     print(n,"*",num,"=",n*num)
+#     num +=1
+
+#07/may
+# n=1
+# while n<=10:
+#     print(n)
+#     n+=1
+
+# program 2 find the sum of first ten natural number
+#
+# n=int(input('Enter the nunber: '))
+# i=1
+# total=0
+# while(i<=n):
+#     total += i
+#     i += 1
+# print('sum =',total)
+
+#making a factorial table wiht using number of 5
+n=int(input("Enter the number: "))
+fact=1
+while (n > 0):
+    fact *= n
+    n-=1
+print(fact)
+
+
+
+
